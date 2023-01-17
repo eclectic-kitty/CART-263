312618ea3df6bb62772bbf345d92318ed7f400eb
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2023-01-17 00:12:33 -0500

    Added grid, started snake head

--- exercise-1/js/script.js
+++ exercise-1/js/script.js
@@ -6,15 +6,24 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+/*
+We need the following features:
+- Grid background
+- Snake head that can move around, only on grid
+- Screen loops
+- Snake segments moving behind
+- Eating apples increases score
+- Increasing number of segments
+- Snake head colliding with segment ends game
+*/
+
 "use strict";
 
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
+let headX = 25;
+let headY = 25;
+let headDir;
+let frameCount = 0;
 
 
 /**
@@ -22,7 +31,6 @@ Description of setup
 */
 function setup() {
     createCanvas(500, 500);
-    background(255, 230, 235);
 }
 
 
@@ -30,5 +38,26 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(215, 190, 255); // Sets background to black
+    let size = 50; // Initiates size variable
 
+    rectMode(CORNER);
+    fill(255, 190, 215); // Sets the colour of the square with a random hue
+    noStroke();
+  
+    for (let y = 0; y < height/size; y++) { // Loop for the y coordinates
+      for (let x = 0; x < width/size; x++) {  // Loop for the x coordinates
+        if (y%2 - x%2 == 0) { // Code only runs every other square space
+          rect(x*size, y*size, size); // Draws a square
+        }
+      }
+    }
+
+    rectMode(CENTER);
+    fill(190, 255, 195);
+    rect(headX, headY, 35, 35, 10);
+
+    if (frameCount = 9) {
+        frameCount = 0
+    }
 }
