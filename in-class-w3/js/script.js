95d31bfd34bb53c449270064e489c9d856698ac9
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2023-01-28 10:40:48 -0500

    Created particle system

--- in-class-w3/js/script.js
+++ in-class-w3/js/script.js
@@ -11,30 +11,59 @@ bla
 "use strict";
 
 
-let ball; // Declare object-holding variable
+let balls = []; // Declare object-holding array
+let types = ["hor", "ver"];
 
 
 // Just initializes canvas
 function setup() {
     createCanvas(500, 500);
-    ball = new Particle();
+    colorMode(HSB);
+    //noStroke();
+
+    // Creates balls
+    for (let i = 0; i < 300; i++) {
+      balls[i] = new Particle(random(width), random(height), types[round(random(1))], random(1, 10), random(5, 50));
+    }
 }
 
 
 function draw() {
-  background(215, 190, 255); // Sets background colour, currently purple
-  ball.display();
+  background(263, 25, 100); // Sets background colour, currently purple
+
+  // For each ball,
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].update(); // moves and displays
+  }
 }
 
 
-// Segments class
+// Balls class
 class Particle {
-  constructor(x, y, xDir, yDir, pos) { 
-    this.x = width/2; // x position
-    this.y = height/2; // y position
+  constructor(x, y, type, maxSpeed, size) { 
+    this.x = x; // x position
+    this.y = y; // y position
+    this.type = type;
+    print(type);
+    this.maxSpeed = maxSpeed;
+    this.size = size;
   }
 
-  display() {
-    ellipse(this.x, this.y, 10);
+  update() {
+    if (this.type == "hor") {
+      let speedX = map(mouseX, 0, width, 0, this.maxSpeed)
+      this.x += random(-speedX, speedX);
+      this.x = constrain(this.x, 0+10, width-10);
+    } else if (this.type == "ver") {
+      let speedY = map(mouseY, 0, width, 0, this.maxSpeed)
+      this.y += random(-speedY, speedY);
+      this.y = constrain(this.y, 0+10, height-10);
+    }
+
+    let d = dist(this.x, this.y, width/2, height/2);
+    d = map(d, 0, 350, 20, 50);
+    fill(230, d, 100);
+
+    ellipse(this.x, this.y, this.size);
   }
 }
