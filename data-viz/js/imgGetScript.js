db174b5628b74097146ef351faa50d640d6a5449
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2023-03-15 00:19:15 -0400

    retooled downloading script

--- data-viz/js/imgGetScript.js
+++ data-viz/js/imgGetScript.js
@@ -2,17 +2,15 @@
 // API URL: http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=fc29072d5368e2cf9e7b26a7b59da0f6&artist=" + + "&album=" + + "&format=json"
 
 let hist;
-let test2;
 let covers = [];
 //let row = 226;
 
 let artist;
 let album;
-let test;
-let imgSize = 1;
+let imgSize = 0;
 let albumNum = 1500;
-
 let albumCount = 0;
+
 let colCounter = 0;
 let displRowCount = 0;
 
@@ -26,7 +24,7 @@ function preload() {
 
 function setup() {
     frameRate(60);
-    createCanvas(510, 510);
+    createCanvas(1020, 1020);
     background(245, 245, 255);
 }
 
@@ -44,18 +42,28 @@ function getAlbumJSON () {
     ready = false;
 
     artist = hist.getString(albumCount, 0);
-    let splitArtist = split(artist, " ");
-    for (i = 0; i < splitArtist.length; i++) {
-        if (splitArtist[i] == '&') {splitArtist[i] = '%26amp;';}
-    }
-    let urlArtist = join(splitArtist, "%20");
+    // let splitArtist = split(artist, " ");
+    // for (i = 0; i < splitArtist.length; i++) {
+    //     if (splitArtist[i] == '&') {splitArtist[i] = '%26';}
+    //     if (splitArtist[i] == '+') {splitArtist[i] = '%2B';}
+    //     if (splitArtist[i] == '`') {splitArtist[i] = ',';}
+    // }
+    // let urlArtist = join(splitArtist, "%20");
+    let urlArtist = artist.replaceAll('&', '%26');
+    urlArtist = urlArtist.replaceAll('+', '%2B');
+    urlArtist = urlArtist.replaceAll('`', ',');
 
     album = hist.getString(albumCount, 1);
-    let splitAlbum = split(album, " ");
-    for (i = 0; i < splitAlbum.length; i++) {
-        if (splitAlbum[i] == '&') {splitAlbum[i] = '%26amp;';}
-    }
-    let urlAlbum = join(splitAlbum, "%20");
+    // let splitAlbum = split(album, " ");
+    // for (i = 0; i < splitAlbum.length; i++) {
+    //     if (splitAlbum[i] == '&') {splitAlbum[i] = '%26';}
+    //     if (splitAlbum[i] == '+') {splitAlbum[i] = '%2B';}
+    //     if (splitAlbum[i] == '`') {splitAlbum[i] = ',';}
+    // }
+    // let urlAlbum = join(splitAlbum, "%20");
+    let urlAlbum = album.replaceAll('&', '%26');
+    urlAlbum = urlAlbum.replaceAll('+', '%2B');
+    urlAlbum = urlAlbum.replaceAll('`', ',');
 
     let url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=fc29072d5368e2cf9e7b26a7b59da0f6&artist=" + urlArtist + "&album=" + urlAlbum + "&format=json";
     loadJSON(url, preparePath, wompWomp);
@@ -75,14 +83,15 @@ function preparePath (albumJSON) {
 function downloadCover (cover) {
     covers.push(album);
     covers.push(cover);
-
     albumCount++;
-    // image(cover, displRowCount * 64, colCounter * 64);
-    // displRowCount++;
-    // if (displRowCount > 7) {
-    //     colCounter++;
-    //     displRowCount = 0;
-    // }
+
+    image(cover, displRowCount * 34, colCounter * 34);
+    displRowCount++;
+    if (displRowCount > 29) {
+        colCounter++;
+        displRowCount = 0;
+    }
+
     ready = true;
     //getAlbumJSON();
 }
