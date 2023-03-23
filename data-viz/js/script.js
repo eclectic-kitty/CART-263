e2372857632d3a010d9efdc51960f5d519a675b8
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2023-03-23 13:38:18 -0400

    Changing between modes

--- data-viz/js/script.js
+++ data-viz/js/script.js
@@ -4,7 +4,7 @@ for CART 263
 
 This script analyses a set of album covers from my own personal listening history
 The average hue, saturation and lightness of each cover is calculated
-The covers can then be arranged along any of these parameters
+The covers can then be displayed along any of these parameters
 */
 
 /*
@@ -14,14 +14,16 @@ Extras:
 - count changing size
 - larger things in the back
 
+When organising by saturation, saturation should not be calculated in black and white pixels
+I think hue doesn't make a big difference if separated
+
 */
 
 let albums = []; // Array to contain album objects
 let hist; // Variable to contain listening history
 let albumInv; // Variable to contain albums index
 let covers = []; // Array to contain cover images
-
-let updated = false;
+let modes = ['lightness', 'saturation'];
 
 function preload() {
     hist = loadTable('assets/listen-history.csv'); // Load listening history
@@ -46,25 +48,97 @@ function setup() {
         console.log(album);
         albums.push(album);
     }
+
+    drawCovers();
 }
 
 function drawCovers() {
+    background(0); // Resets background
+
     // Display albums
     for (let i = albums.length - 1; i > 0; i--) { // Runs once per album
-        let hueRad = 400; // Center of hue circle
+        let hueRad = 300; // Center of hue circle
         let hueCenter = createVector(width / 2, height / 2); // Variable for the center of the circle of album covers
     
-        let posVec = createVector(sin(albums[i].avgHue) * hueRad, cos(albums[i].avgHue) * hueRad); // 
-        posVec = posVec.mult(map(albums[i].avgLight, 0, 100, 0.5, 1.5));
+        let posVec = createVector(sin(albums[i].avgHue) * hueRad, cos(albums[i].avgHue) * hueRad); // Sets x and y position along circle of album cover by calculating vector from the hue (angle)
+
+        // The 'if' statement here separates the two modes
+        if (modes[0] == 'lightness') {
+            posVec = posVec.mult(map(albums[i].avgLight, 0, 100, 2.5, 0)) // Varies magnitude of vector, ie. distance from center based on lightness. Darkest at edges, lightest at center
+        } else if (modes[0] == 'saturation') {
+            posVec = posVec.mult(map(albums[i].avgSat, 0, 100, 0, 2.5)); // Same as above, but based on saturation. Most saturated at edges, least in center.
+        }
+
         posVec.add(hueCenter); // Places circle at defined center
 
         image(albums[i].cover, posVec.x, posVec.y); // Display cover
     }
 }
 
+//Changes mode when spacebar is pressed
 function keyPressed() {
-    if (keyCode == SPACE_BAR) {
-        console.log('boo');
+    if (keyCode == 32) {
+        modes.reverse(); // Changes mode
+        drawCovers();
+    }
+}
+
+class Album {
+    constructor(index) {
+        this.cover = covers[index]; // Set cover
+        
+        let rawTitle = albumInv.getString(index, 1); // Get title from albums.csv
+        // Remove special characters used for API calls in setup script
+        rawTitle = rawTitle.replaceAll('`', '');
+        rawTitle = rawTitle.replaceAll('^', '');
+        this.title = rawTitle; // Set title
+
+        let avgColour = this.calcAvgCol() // Gets array with cover's average colour in HSL
+
+        this.avgHue = avgColour[0];
+        this.avgSat = avgColour[1];
+        this.avgLight = avgColour[2];
+
+        this.count = 0;
+    }
+
+    // Calculates cover's average colour, returns array with values in HSL
+    calcAvgCol() {
+        this.cover.loadPixels(); // Loads pixels from image
+        let pix = this.cover.pixels; // reference to pixels
+        //console.log(pix);
+
+        // Initialising variables to keep track of hsl totals for avg calculation
+        let saturations = 0;
+        let lightnesses = 0;
+        // Hue is different as it's not a linear value from 1 - 100, but a degree on a circle
+        // These angles can be seen as vectors on a unit circle though
+        // The average vector of these, turned back into an angle, is then the answer I'm lookinn for
+        let hues = createVector(0, 0);
+
+        for (let i = 0; i < (pix.length / 4); i++) { // runs once per pixel
+            let r = pix[i*4]; // gets red value
+            let g = pix[i*4+1]; // gets green value
+            let b = pix[i*4+2]; // gets blue value
+            let pixCol = color(r, g, b); // creating a colour object from rgb values. This is done so the HSL values can be calculated from the RGB values providede by the .pixels array
+
+            lightnesses += lightness(pixCol); // Adds lightness of current pixel
+            if (lightness(pixCol) > 5 && lightness(pixCol) < 95) { // If pixel isn't black or white,
+                hues.add(createVector(cos(hue(pixCol)), sin(hue(pixCol)))) // Adds hue of current pixel
+                saturations += saturation(pixCol); // Adds saturation of current pixel
+
+                // The saturation and hue of black and white pixels aren't considered in the average so that in covers with bright coloured objects and imperceptibly coloured black or white backgrounds, the backgrounds don't impact the calculation
+                // This is also done for saturation so that when varying position along saturation, similarly imperceptibly saturated blacks and whites don't place mostly black and white images in the more saturated parts of the circle
+            }
+        }
+
+        // I don't fully understand the math here, ie. how atan2 gets the angle of the sum of the vectors
+        // This is where I got it from though: https://www.themathdoctors.org/averaging-angles/
+        let avgHue = atan2(hues.y, hues.x); 
+        if (avgHue < 0) {avgHue += 360;} // atan2 returns an angle from -180 to 180, so this wraps the negative values back around
+        let avgSat = saturations / (pix.length / 4); // Calculating average saturation
+        let avgLight = lightnesses / (pix.length / 4); // Calculating average lightness
+        return [avgHue, avgSat, avgLight]; // Return colour
     }
 }
 
@@ -121,60 +195,3 @@ function keyPressed() {
 //     // }
 //     //console.log(albums[1]);
 // }
-
-class Album {
-    constructor(index) {
-        this.cover = covers[index]; // Set cover
-        
-        let rawTitle = albumInv.getString(index, 1); // Get title from albums.csv
-        // Remove special characters used for API calls in setup script
-        rawTitle = rawTitle.replaceAll('`', '');
-        rawTitle = rawTitle.replaceAll('^', '');
-        this.title = rawTitle; // Set title
-
-        let avgColour = this.calcAvgCol() // Gets array with cover's average colour in HSL
-
-        this.avgHue = avgColour[0];
-        this.avgSat = avgColour[1];
-        this.avgLight = avgColour[2];
-
-        this.count = 0;
-    }
-
-    // Calculates cover's average colour, returns array with values in HSL
-    calcAvgCol() {
-        this.cover.loadPixels(); // Loads pixels from image
-        let pix = this.cover.pixels; // reference to pixels
-        //console.log(pix);
-
-        // Initialising variables to keep track of hsl totals for avg calculation
-        let saturations = 0;
-        let lightnesses = 0;
-        // Hue is different as it's not a linear value from 1 - 100, but a degree on a circle
-        // These angles can be seen as vectors on a unit circle though
-        // The average vector of these, turned back into an angle, is then the answer I'm lookinn for
-        let hues = createVector(0, 0);
-
-        for (let i = 0; i < (pix.length / 4); i++) { // runs once per pixel
-            let r = pix[i*4]; // gets red value
-            let g = pix[i*4+1]; // gets green value
-            let b = pix[i*4+2]; // gets blue value
-            let pixCol = color(r, g, b); // creating a colour object from rgb values
-
-            lightnesses += lightness(pixCol); // Adds lightness of current pixel
-            //saturations += round(saturation(pixCol)); // Adds together saturations for displaying by saturation
-
-            if (lightness(pixCol) > 10 && lightness(pixCol) < 90) { // If pixel isn't black or white
-                hues.add(createVector(cos(hue(pixCol)), sin(hue(pixCol)))) // Adds hue of current pixel
-                saturations += saturation(pixCol); // Adds saturation of current pixel
-            }
-        }
-        // I don't fully understand the math here, ie. how atan2 gets the angle of the sum of the vectors
-        // This is where I got it from though: 
-        let avgHue = atan2(hues.y, hues.x); 
-        if (avgHue < 0) {avgHue += 360;} // atan2 returns an angle from -180 to 180, so this wraps the negative values back around
-        let avgSat = saturations / (pix.length / 4); // Calculating average saturation
-        let avgLight = lightnesses / (pix.length / 4); // Calculating average lightness
-        return [avgHue, avgSat, avgLight]; // Return colour
-    }
-}
